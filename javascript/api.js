a90f565686dc642d718adc24e292f4c9b8c2cc5c
Zohra Fredericks <zohrafr@gmail.com>
2025-10-21 15:29:03 +0200

    Update api.js

--- javascript/api.js
+++ javascript/api.js
@@ -6,46 +6,40 @@ const girlBtn = document.getElementById("girlBtn");
 const boyBtn = document.getElementById("boyBtn");
 const doll = document.getElementById("doll");
 
-// -------------------- CLOTHES ARRAYS --------------------
-const girlClothes = [
+// -------------------- UNISEX CLOTHES --------------------
+const unisexClothes = [
   "dress.png","browntop.png","trouserb.png","hatbrown.png",
-  "lightbrownshoe.png","greentee.png","girlskirt1.png",
-  "top.png","top1.png","ballgown1.png","brownjersey1.png"
+  "lightbrownshoe.png","lightbrownshoe2.png","greentee.png",
+  "girlskirt1.png","top.png","top1.png","ballgown1.png",
+  "brownjersey1.png","blueshirt1.png","jerseygreen.png",
+  "boyshoelt.png","boyshoert.png","applet.png","onez.png",
+  "brownt.png","darkbrownshoe1.png","darkbrownshoe2.png",
+  "darkbrownshoe3.png","darkbrownshoe4.png"
 ];
 
-const boyClothes = [
-  "boyshirt1.png","boytrousers1.png","boyshoelt.png","boyshoert.png",
-  "jerseygreen.png","darkbrownshoe1.png","darkbrownshoe2.png",
-  "darkbrownshoe3.png","darkbrownshoe4.png","applet.png","onez.png","brownt.png"
-];
-
-// -------------------- DRAGGABLE LOGIC --------------------
+// -------------------- DRAG & DROP --------------------
 function addDragEvents(item) {
   item.addEventListener("dragstart", (e) => {
     e.dataTransfer.setData("text/plain", null);
     item.classList.add("dragging");
   });
-  item.addEventListener("touchstart", () => {
-    item.classList.add("dragging");
-  });
+  item.addEventListener("touchstart", () => item.classList.add("dragging"));
 }
 
 function enableDragForAll() {
   document.querySelectorAll(".draggable").forEach(addDragEvents);
 }
 
-// -------------------- DROP AREA --------------------
-function dropItem(x, y){
+function dropItem(x, y) {
   const dragging = document.querySelector(".dragging");
-  if(dragging){
-    const rect = dollArea.getBoundingClientRect();
-    const clone = dragging.cloneNode(true);
-    clone.style.position = "absolute";
-    clone.style.left = `${x - rect.left - dragging.width/2}px`;
-    clone.style.top = `${y - rect.top - dragging.height/2}px`;
-    clone.classList.remove("dragging");
-    dollArea.appendChild(clone);
-  }
+  if (!dragging) return;
+  const rect = dollArea.getBoundingClientRect();
+  const clone = dragging.cloneNode(true);
+  clone.style.position = "absolute";
+  clone.style.left = `${x - rect.left - dragging.width/2}px`;
+  clone.style.top = `${y - rect.top - dragging.height/2}px`;
+  clone.classList.remove("dragging");
+  dollArea.appendChild(clone);
 }
 
 dollArea.addEventListener("dragover", (e) => e.preventDefault());
@@ -58,38 +52,10 @@ dollArea.addEventListener("touchend", (e) => {
   dropItem(touch.clientX, touch.clientY);
 });
 
-// -------------------- HIGHLIGHT DOLL AREA --------------------
-dollArea.addEventListener("dragenter", () => {
-  dollArea.style.borderColor = "#c94e8a";
-  dollArea.style.background = "#fff0f8";
-});
-dollArea.addEventListener("dragleave", () => {
-  dollArea.style.borderColor = "#aaa";
-  dollArea.style.background = "#fff";
-});
-document.addEventListener("touchmove", (e) => {
-  const dragging = document.querySelector(".dragging");
-  if (!dragging) return;
-  const touch = e.touches[0];
-  const rect = dollArea.getBoundingClientRect();
-  if(touch.clientX > rect.left && touch.clientX < rect.right &&
-     touch.clientY > rect.top && touch.clientY < rect.bottom){
-    dollArea.style.borderColor = "#c94e8a";
-    dollArea.style.background = "#fff0f8";
-  } else {
-    dollArea.style.borderColor = "#aaa";
-    dollArea.style.background = "#fff";
-  }
-});
-dollArea.addEventListener("drop", () => {
-  dollArea.style.borderColor = "#aaa";
-  dollArea.style.background = "#fff";
-});
-
 // -------------------- LOAD CLOTHES --------------------
-function loadClothes(clothesArray){
+function loadClothes(clothesArray) {
   clothesArea.innerHTML = "";
-  clothesArray.forEach((img)=>{
+  clothesArray.forEach((img) => {
     const el = document.createElement("img");
     el.src = `images/${img}`;
     el.alt = img;
@@ -102,7 +68,7 @@ function loadClothes(clothesArray){
 
 // -------------------- RESET FUNCTION --------------------
 resetBtn.addEventListener("click", () => {
-  document.querySelectorAll(".doll-area img:not(#doll)").forEach(i=>i.remove());
+  document.querySelectorAll(".doll-area img:not(#doll)").forEach(i => i.remove());
 });
 
 // -------------------- TOGGLE DOLLS --------------------
@@ -110,19 +76,15 @@ girlBtn.addEventListener("click", () => {
   doll.src = "images/lightgirl.png";
   girlBtn.classList.add("active");
   boyBtn.classList.remove("active");
-  loadClothes(girlClothes);
-  resetBtn.click();
 });
 
 boyBtn.addEventListener("click", () => {
   doll.src = "images/boy1.png";
   boyBtn.classList.add("active");
   girlBtn.classList.remove("active");
-  loadClothes(boyClothes);
-  resetBtn.click();
 });
 
 // -------------------- INITIAL LOAD --------------------
-loadClothes(girlClothes);
+loadClothes(unisexClothes); // single clothes panel for both dolls
 
 
